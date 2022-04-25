2cd5581b2825471a2a1ef17734c18b8c745495e8
ZeroRin <pptxp@qq.com>
2022-04-25 15:31:15 +0800

    add chdir to set cwd

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from utils import *
 import pulp as lp
 import argparse
 import os
+from pathlib import Path
 
+os.chdir(Path(__file__).resolve().parent)
 # %% argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('theater_id',default='748',type=str,help='关卡id,如736代表第7期高级区第6关')
